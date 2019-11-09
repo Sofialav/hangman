fc79b292c9c8e03bb5796e1791cf494960c30613
Sofia <52375909+Sofialav@users.noreply.github.com>
2019-11-09 17:54:52 +0100

    added hangman.py file

---
+++ hangman.py
@@ -0,0 +1,89 @@
+import random
+
+#function for hangman drawing in 10 incorrect attempts
+def draw(missed):
+    if missed == 1:
+        print("  0 ")
+    elif missed == 2:
+        print("  0 ")
+        print("  | ")
+    elif missed == 3:
+        print("  0 ")
+        print("  | ")
+        print(" /  ")
+    elif missed == 4:
+        print("  0  ")
+        print("  |  ")
+        print(" / \ ")
+    elif missed == 5:
+        print(" \0  ")
+        print("  |  ")
+        print(" / \ ")
+    elif missed == 6:
+        print(" \0/ ")
+        print("  |  ")
+        print(" / \ ")
+    elif missed == 7:
+        print("      | ")
+        print(" \0/  | ")
+        print("  |   | ")
+        print(" / \    ")
+    elif missed == 8:
+        print("      | ")
+        print(" \0/  | ")
+        print("  |   | ")
+        print(" / \ / \ ")
+    elif missed == 9:
+        print("  ----- ")
+        print("      | ")
+        print(" \0/  | ")
+        print("  |   | ")
+        print(" / \ / \ ")
+    elif missed == 10:
+        print("  ----- ")
+        print("  |   | ")
+        print("  0   | ")
+        print(" /|\  | ")
+        print(" / \ / \ ")
+
+def hangman():
+    wordlist = ["tiger", "badger", "beaver", "cougar", "eagle", "elephant", "flamingo", "gorilla", "lobster", "pokemon"]
+    word = random.choice(wordlist)
+    #validation for user input letters:
+    valletters = "abcdefghijklmnopqrstuvwxyz"
+    #create a duplicate of the word and mask letters with stars:
+    letter = ""
+    for i in word:
+        letter += "*"
+    #user attempts countdown:
+    attempt = 10
+    #incorrect attempts counter:
+    missed = 0
+    print(letter)
+    #loop if wrong attempts<10 and have unopened letters in the world
+    while (missed < 10) and ("*" in letter):
+        print("Mistakes left: ", attempt)
+        usrletter = input("\nPlease enter a letter: ")
+        if usrletter.lower() not in valletters:
+            print("Incorrect input! You need to enter a single letter")
+        if usrletter in word:
+            index = word.find(usrletter)
+            letter = letter[:index]+usrletter+letter[index+1:]
+            word = word[:index]+"-"+word[index+1:]
+            print(letter)
+        else:
+            print(letter)
+            attempt = attempt - 1
+            missed += 1
+            draw(missed)
+    else:
+        if ("*" not in letter):
+            print("\nCongratulations! You win!")
+        else:
+            print("\nSorry, you are dead.")
+
+name = input("Please enter your name: ")
+print("Hi,", name)
+print("Try to guess the word. After 10 mistakes you are dead.")
+print("Every star is a letter.")
+hangman()
